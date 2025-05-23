65d721e162b8a88e1c9ecf68c00f6cc60a45b834
Hassnae Chlili <hassnaechlili48@gmail.com>
2025-05-23 14:05:38 +0200

    activitat

---
+++ BF3/E1/js/a1.js
@@ -0,0 +1,7 @@
+const cita = ("Compartir el conocimiento es el acto más fundamental de la amistad. Porque es una forma de dar algo sin perder nada.");
+
+const citaMayusculas = cita.toUpperCase();
+console.log(citaMayusculas);
+
+const citaMinusculas = cita.toLowerCase();
+console.log(citaMinusculas);
---
+++ BF3/E1/js/a2.js
@@ -0,0 +1,4 @@
+const anionacimiento = 1958;
+const anioactual = 2025;
+var edad = anioactual - anionacimiento;
+console.log('Madonna tiene ' + edad + ' anios');
